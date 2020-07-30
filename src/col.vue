8f709afa64751b1cea9e93c275939179d78bdcd2
linzheng <alidlg9045@126.com>
2020-07-30 17:59:52 +0800

    完成 row col 的雏形

---
+++ src/col.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="col" :class="[`col-${span}`]">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+  export default {
+    props:{
+      span:{
+        type:[Number,String]
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .col{
+    height: 100px;
+    background: grey;
+    width: 50%;
+    border: 1px solid red;
+    $class-prefix: col-;//class前缀
+    @for $n from 1 through 24{
+      &.#{$class-prefix}#{$n}{//scss的#和js的$同理
+        width: ($n / 24) * 100%;
+      }
+    }
+  }
+</style>
---
+++ src/row.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="row">
+    <slot/>
+  </div>
+</template>
+
+<script lang="ts">
+  export default {};
+</script>
+
+<style lang="scss" scoped>
+  .row {
+    display: flex;
+  }
+</style>
