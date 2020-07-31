4db9883af865f45c312f01f914d0c580ae309c28
linzheng <alidlg9045@126.com>
2020-07-31 16:10:23 +0800

    进一步完善row col 添加align 横向对齐左中右

--- src/col.vue
+++ src/col.vue
@@ -15,35 +15,23 @@
       }
     },
     data(){
-      return{
-        gutter: 0
-      }
+      return{gutter: 0}
     },
     computed: {//计算出来的值，一个变量变了，我也要变就用computed，就拿下面的gutter，是会变得，所以data不行，要用computed
       colClass(){
         let {span,offset} = this
-        return [
-          span && `col-${span}`,
-          offset && `offset-${offset}`
-        ]
+        return [span && `col-${span}`, offset && `offset-${offset}`]
       },
       colStyle(){
-        return{
-          paddingLeft: this.gutter/2+'px',
-          paddingRight: this.gutter/2+'px'
-        }
+        return{paddingLeft: this.gutter/2+'px', paddingRight: this.gutter/2+'px'}
       }
     }
   }
-  var div = document.createElement('div')
-  var childDiv = document.createElement('div')
-  div.appendChild(childDiv)
-  document.body.appendChild(div)
 </script>
 
 <style lang="scss" scoped>
   .col{
-    width: 50%;
+    border: 1px solid red;
     $class-prefix: col-;//class前缀
     @for $n from 1 through 24{
       &.#{$class-prefix}#{$n}{//scss的#和js的$同理
--- src/row.vue
+++ src/row.vue
@@ -1,36 +1,38 @@
 <template>
-  <div class="row" :style="rowStyle">
+  <div class="row" :style="rowStyle" :class="rowClass">
     <slot/>
   </div>
 </template>
-
 <script lang="ts">
   export default {
     props: {
       gutter: {
         type: [Number, String]
+      },
+      align: {
+        type: String,
+        validator(value){return ['left','right','center'].includes(value)}//检查器，检查align的值是不是这个数组里面的值
       }
     },
     computed:{
-      rowStyle(){
-        return {
-          marginLeft: -this.gutter/2+'px',
-          marginRight: -this.gutter/2+'px'
-        }
+      rowStyle(){return {marginLeft: -this.gutter/2+'px', marginRight: -this.gutter/2+'px'}},
+      rowClass(){
+        let {align} = this
+        return [align && `align-${align}`]
       }
     },
     mounted(){
-      console.log(this.$children);
-      this.$children.forEach((vm) => {
+      this.$children.forEach((vm) => { //在row这个div下面的孩子，把vm遍历出来，如果row上面有gutter，就加上gutter
         vm.gutter = this.gutter
       })
     },
   };
 </script>
-
 <style lang="scss" scoped>
   .row {
-    display: flex;
-    margin: 0 -10px;
+    display: flex;margin: 0 -10px;
+    &.align-left{justify-content: start;}
+    &.align-right{justify-content: flex-end;}
+    &.align-center{justify-content: center;}
   }
 </style>
