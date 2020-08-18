5666d747ff93de28c0148b6094ccbc9867a0825c
linzheng <alidlg9045@126.com>
2020-08-18 12:56:53 +0800

    制作官网 存在bug

--- docs/.vuepress/components/button-demos.vue
+++ docs/.vuepress/components/button-demos.vue
@@ -1,20 +1,10 @@
 <template>
   <div>
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 2185373cad9b40000da5bd3fab3d6e41ebb5aad1
     <g-button>默认按钮</g-button>
     <g-button icon="settings">默认按钮</g-button>
     <g-button icon="settings" icon-position="right">默认按钮</g-button>
     <g-button :loading="true">默认按钮</g-button>
-<<<<<<< HEAD
     <g-button>默认设置</g-button>
-=======
-=======
-    <g-button>默认设置</g-button>
->>>>>>> 95e178e162c760f43e86136d9cca02e21a520c67
->>>>>>> 2185373cad9b40000da5bd3fab3d6e41ebb5aad1
   </div>
 </template>
 
